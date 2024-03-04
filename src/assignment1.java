035771170868a3a251c3f35f3fe495155a9b0d46
pravallika9702 <sxg20201@ucmo.edu>
2024-03-05 05:23:52 +0530

    commit

---
+++ src/assignment1.java
@@ -0,0 +1,8 @@
+public class assignment1 {
+    public static void main(String[] args) {
+        double value1 = 123.456;
+        int value2 = (int) value1;
+
+        System.out.println(value2);
+    }
+}
