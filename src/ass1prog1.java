f3b95908ad6c3bd7cbca4cc7326629683da0b04c
pravallika9702 <sxg20201@ucmo.edu>
2024-03-07 03:51:12 +0530

    commit

--- src/ass1prog1.java
+++ src/ass1prog1.java
@@ -2,7 +2,6 @@ public class ass1prog1 {
     public static void main(String[] args) {
         short value1 = 123;
         long value2 = (int) value1;
-
         System.out.println(value2);
     }
 }
