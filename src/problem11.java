9b14d312d9b3c89bf7a940eec5995cc9432fc0b6
pravallika9702 <sxg20201@ucmo.edu>
2024-03-08 04:03:36 +0530

    commit

---
+++ src/problem11.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class problem11 {
+    public static void main(String args[]) {
+        int num=0;
+
+
+        for(int i=1; i <=10; i++) {
+            System.out.println(i);
+
+        }
+    }
+}
